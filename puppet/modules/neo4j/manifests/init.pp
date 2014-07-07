7e231adeba823d9f881406b07b837831b0eefbd2
Bill Heughan <billheughan@gmail.com>
2014-07-07 03:31:16 -0400

    Actually added neo4j puppet module

---
+++ puppet/modules/neo4j/manifests/init.pp
@@ -0,0 +1,27 @@
+class neo4j
+{
+
+    apt::key { 'neo4j':
+        ensure => 'present',
+        key_source => 'http://debian.neo4j.org/neotechnology.gpg.key'
+    }
+
+    file_line { 'sudo_rule':
+        path => '/etc/apt/sources.list',
+        line => 'deb http://debian.neo4j.org/repo stable/',
+    }
+    
+    package { "neo4j":
+        ensure => latest,
+        require => Exec['apt-get update'],
+        logoutput => true
+    }
+
+    exec
+    {
+        command => "/bin/bash -c 'neo4j start'",
+        require => Package["neo4j"],
+        timeout => 5000,
+        logoutput => true
+    }
+}
