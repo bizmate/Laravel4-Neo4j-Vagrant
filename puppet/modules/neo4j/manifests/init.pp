058df66116cddfca80c6e1a7c5aaf2777f3770cb
Bill Heughan <billheughan@gmail.com>
2014-07-07 03:36:58 -0400

    added missing resource names

--- puppet/modules/neo4j/manifests/init.pp
+++ puppet/modules/neo4j/manifests/init.pp
@@ -11,17 +11,17 @@ class neo4j
         line => 'deb http://debian.neo4j.org/repo stable/',
     }
     
-    package { "neo4j":
+    package { 'neo4j':
         ensure => latest,
         require => Exec['apt-get update'],
         logoutput => true
     }
 
-    exec
-    {
+    exec { 'start neo4j':
         command => "/bin/bash -c 'neo4j start'",
         require => Package["neo4j"],
         timeout => 5000,
         logoutput => true
     }
+
 }
